e30a8127ee8f3ce135a715eb6d34724b25fe1896
Pedro Phillipe <126290288+pedrophillipedev@users.noreply.github.com>
2024-12-06 13:43:15 -0300

    ClubFC

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "344796185"
+    serviceWorkerVersion: "283857653"
   }
 });
