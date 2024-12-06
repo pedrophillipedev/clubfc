5705f79c6d6bff225051f7c775ec3f643735e4e6
Pedro Phillipe <126290288+pedrophillipedev@users.noreply.github.com>
2024-12-06 15:38:44 -0300

    ClubFC - versão funcional

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "ddd49e4db6dc26bd37e76073312977af
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "dc0c6c0e3d9e08f95a135a40b97a10f8",
+"flutter_bootstrap.js": "aa039a0015396c0d904bfac98681393c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "43686ecaf67546eaac42424dbd9a4b11",
-"/": "43686ecaf67546eaac42424dbd9a4b11",
-"main.dart.js": "87b0979d3af4a6e7bef2270c6740c4e1",
+"index.html": "f540a71f1dc02fb9d513378970c7edf1",
+"/": "f540a71f1dc02fb9d513378970c7edf1",
+"main.dart.js": "59d74265f65c707d517a0e8c3de7091b",
 "manifest.json": "ad5663af2993be34b18e216ae66c3b13",
 "version.json": "fc5d806a25188323fd0f70e669dac3c4"};
 // The application shell files that are downloaded before a service worker can
