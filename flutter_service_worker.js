64db4f77aa956836bdfa2dd2b8004280ea92e775
Pedro Phillipe <126290288+pedrophillipedev@users.noreply.github.com>
2024-12-06 15:43:29 -0300

    ClubFC - agora sim rs

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ddd49e4db6dc26bd37e76073312977af
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "bea27e1a9d1be1c498f9a8135ce7a9ed",
+"flutter_bootstrap.js": "9acc363ad93b1c631e649c2db6b74a66",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
