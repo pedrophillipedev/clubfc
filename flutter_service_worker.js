089c1596666b5d3ae5d4950c99e547f1472b0432
Pedro Phillipe <126290288+pedrophillipedev@users.noreply.github.com>
2024-12-06 19:57:29 -0300

    ClubFC - atualização barra de rolagem

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "60b2909bc9955286984fcd066ed2e654
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "f477d7be5916508fff6a91651de470ee",
+"flutter_bootstrap.js": "880681f257a3dec91d40693f292d74f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
