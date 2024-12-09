e7cdc47ead6fbc71de0673ecdbb59563aac0f50d
Pedro Phillipe <126290288+pedrophillipedev@users.noreply.github.com>
2024-12-09 09:54:08 -0300

    ClubFC - atualização home
    Container no body pra preencher toda tela

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,14 +29,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "60b2909bc9955286984fcd066ed2e654
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "257b7987512f99febfd1f43d2f0def4a",
+"flutter_bootstrap.js": "0385910fde2f6ef71abee4c6bdc09982",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "296bad76caed48f3b28cebbcc853cbba",
 "/": "296bad76caed48f3b28cebbcc853cbba",
-"main.dart.js": "67560800c8f775cd1dd83ca6d31338a6",
+"main.dart.js": "14bbbbb5a3a6a946967379265ec957a7",
 "manifest.json": "ad5663af2993be34b18e216ae66c3b13",
 "version.json": "fc5d806a25188323fd0f70e669dac3c4"};
 // The application shell files that are downloaded before a service worker can
